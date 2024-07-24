8a1b126071b942c1f34582ba4896e1f09438633c
Mihiran Nanayakkara <nilanineranjika@gmail.com>
2024-07-24 23:47:18 +0530

    Create ProfileActivity.kt

---
+++ BodimaApp/app/src/main/java/com/example/bodimaapp/ProfileActivity.kt
@@ -0,0 +1,40 @@
+package com.example.bodimaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile) // Set the layout {
+
+        val nextButton1 = findViewById<ImageView>(R.id.homebutton1)
+        nextButton1.setOnClickListener {
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        val nextButton2 = findViewById<ImageView>(R.id.addpostbutton1)
+        nextButton2.setOnClickListener {
+
+            val intent = Intent(this, AddpostActivity::class.java)
+            startActivity(intent)
+        }
+        val nextButton3 = findViewById<ImageView>(R.id.savebutton1)
+        nextButton3.setOnClickListener {
+
+            val intent = Intent(this, SavedActivity::class.java)
+            startActivity(intent)
+        }
+        val nextButton4 = findViewById<ImageView>(R.id.supportbutton1)
+        nextButton4.setOnClickListener {
+
+            val intent = Intent(this, supportActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
